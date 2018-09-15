f607dcdada56422a5f1f67218709e6e6e32251bd
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2018-09-15 18:18:54 +0200

    Send messages to customers

--- send_sms/views.py
+++ send_sms/views.py
@@ -1,5 +1,7 @@
 import csv
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from twilio.rest import Client
 
 from .forms import MessageForm
 
@@ -14,11 +16,29 @@ def get_customers_cellphones(csv_file):
     return customers_cellphones
 
 
+def send_message_to_customers(phones, message_body):
+    message_sids = []
+    for phone in phones:
+        account_sid = 'AC4b174ed56fd13054ac1ff1172df80083'
+        auth_token = 'a10edaf2835481d9b825ff10a8ec3776'
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            from_='+18508057647',
+            body=message_body,
+            to=phone
+        )
+        message_sids.append(message.sid)
+    return message_sids
+
+
 def home_view(request):
     if request.method == 'POST':
         form = MessageForm(request.POST, request.FILES)
         if form.is_valid():
-            print(get_customers_cellphones(request.FILES['file']))
+            customers_cellphones = get_customers_cellphones(request.FILES['file'])
+            message_body = form.cleaned_data.get('message')
+            print(send_message_to_customers(customers_cellphones, message_body))
+            return redirect('send_sms:home')
     else:
         form = MessageForm()
     return render(request, 'send_sms/home.html', {'form': form})
