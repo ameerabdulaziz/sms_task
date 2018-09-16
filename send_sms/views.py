4227541acf0a366036604887446698fb5ab524b8
Ameer Abdulaziz <ameer.abdulaziz93@gmail.com>
2018-09-16 16:42:53 +0200

    Save the message detail for each customer

--- send_sms/views.py
+++ send_sms/views.py
@@ -7,14 +7,16 @@ from twilio.rest import Client
 from .forms import MessageForm
 
 
-def get_customers_cellphones(csv_file):
+def get_customers_info(csv_file):
     data = csv_file.read().decode('utf-8').splitlines()
     reader = csv.reader(data)
     next(reader)
     customers_cellphones = []
+    customers_names = []
     for row in reader:
         customers_cellphones.append(row[1])
-    return customers_cellphones
+        customers_names.append(row[0])
+    return customers_names, customers_cellphones
 
 
 def send_message_to_customers(phones, message_body):
@@ -32,15 +34,26 @@ def send_message_to_customers(phones, message_body):
     return message_sids
 
 
+def save_message_detail(names, phones, message, sids):
+    with open('message_detail.csv', 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        headers = ['Name', 'Cellphone number', 'Message', 'Message_sid', 'Status', 'Sent_date-time']
+        csv_writer.writerow(headers)
+        for name, phone, sid in zip(names, phones, sids):
+            csv_writer.writerow([name, phone, message, sid])
+
+
 def home_view(request):
     if request.method == 'POST':
         form = MessageForm(request.POST, request.FILES)
         if form.is_valid():
-            customers_cellphones = get_customers_cellphones(request.FILES['file'])
+            customers_names, customers_cellphones = get_customers_info(request.FILES['file'])
             message_body = form.cleaned_data.get('message')
-            print(send_message_to_customers(customers_cellphones, message_body))
+            sids = send_message_to_customers(customers_cellphones, message_body)
+            save_message_detail(customers_names, customers_cellphones, message_body, sids)
             messages.success(request, 'Message sent!')
             return redirect('send_sms:home')
     else:
         form = MessageForm()
+
     return render(request, 'send_sms/home.html', {'form': form})
